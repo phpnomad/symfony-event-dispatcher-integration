3d05d409d250cf8edf5da81299eed2b63a89f804
Alex Standiford <alex@standiford.us>
2024-09-28 12:10:43 -0400

    Adds required di package

--- lib/Initializer.php
+++ lib/Initializer.php
@@ -2,12 +2,16 @@
 
 namespace PHPNomad\Symfony\Component\EventDispatcherIntegration;
 
+use PHPNomad\Di\Interfaces\CanSetContainer;
+use PHPNomad\Di\Traits\HasSettableContainer;
 use PHPNomad\Events\Interfaces\ActionBindingStrategy;
 use PHPNomad\Events\Interfaces\EventStrategy;
 use PHPNomad\Loader\Interfaces\HasClassDefinitions;
 
-class Initializer implements HasClassDefinitions
+class Initializer implements HasClassDefinitions, CanSetContainer
 {
+    use HasSettableContainer;
+
     public function getClassDefinitions(): array
     {
         return [
